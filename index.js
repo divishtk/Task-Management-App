97154f3e81ccf98cfc56774216ab4a14e7c295b9
divishtk <divisht.avion@hotmail.com>
2021-06-28 13:43:49 +0530

    Delete Function Done

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
 console.log("JS Working");
 
-const cardsContainer = document.querySelector(".cards_container");
+const cardsContainer = document.querySelector(".cards_container"); //fetching the row
 //console.log(cardsContainer);
 
-const globalStore = [];
-
-const saveChanges = () => {
-  const taskData = {
-    id: `${Date.now()}`, //generates the unique id (Date,now)
-    imageUrl: document.getElementById("imageurl").value,
-    taskTitle: document.getElementById("tasktitle").value,
-    taskType: document.getElementById("tasktype").value,
-    taskDescription: document.getElementById("taskdesc").value,
-  };
-
-  console.log(taskData);
-
-  
-  cardsContainer.insertAdjacentHTML("beforeend", generateNewCards(taskData));
-
-  globalStore.push(taskData);
-
-  localStorage.setItem("taskY", JSON.stringify({ cards: globalStore })); //json.stringify accepts an object ie cards
-  //  console.log(cardsContainer)
-
-  
-};
-
-
+let globalStore = [];
 
 
 
 const generateNewCards = (taskData) => `
-<div class="col-md-6 col-lg-4" id="${taskData.id}">
+<div class="col-md-6 col-lg-4">
 <div class="card text-center">
   <div class="card-header d-flex justify-content-end">
     <button type="button" class="btn btn-outline-success mr-2">
       <i class="fas fa-pencil-alt"></i>
     </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="fas fa-trash-alt"></i>
+    <button type="button" class="btn btn-outline-danger" id="${taskData.id}" onclick="deleteCard.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id="${taskData.id}" onclick="deleteCard.apply(this, arguments)"></i>
     </button>
   </div>
   <img class="card-img-top" src="${taskData.imageUrl}" style:"width:100%"
@@ -69,6 +45,43 @@ const generateNewCards = (taskData) => `
 
 
 
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, //generates the unique id (Date,now)
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdesc").value,
+  };
+
+  console.log(taskData);
+
+  
+  cardsContainer.insertAdjacentHTML("beforeend", generateNewCards(taskData));
+
+  globalStore.push(taskData);
+
+  localStorage.setItem("taskY", JSON.stringify({ cards: globalStore })); //json.stringify accepts an object ie cards
+  //  console.log(cardsContainer)
+
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const loadCardData = () => {
     //will call localstorge to get tasky card data
 
@@ -79,14 +92,11 @@ const loadCardData = () => {
     const { cards } = JSON.parse(getCardFromLocalStorage);
 
     //loop over those  array of task objects  to create HTML cards and inject it to DOM
-    cards.map((cardObject) => {
-      cardsContainer.insertAdjacentHTML(
-        "beforeend",
-        generateNewCards(cardObject)
-      );
+    cards.map((cardObject) => { cardsContainer.insertAdjacentHTML("beforeend",generateNewCards(cardObject));
 
       //update globalStorage
       globalStore.push(cardObject);
+
     });
   };
 
@@ -97,3 +107,56 @@ const loadCardData = () => {
 //Interface as middleman(inerface is something  to interact with applications progamtically)
 
 //localstorage -->with some methods(Add,delete,update) -->js
+
+
+
+
+
+
+const deleteCard=(event)=>{
+  event=window.event;
+
+  //id
+  const targetID=event.target.id;
+
+  const tagname=event.target.tagName;
+
+
+
+  
+
+
+
+    //newwely updated array
+      globalStore=globalStore.filter((cardObject)=>cardObject.id !== targetID);
+
+      localStorage.setItem("taskY",JSON.stringify({cards:globalStore}));
+
+
+      
+
+        //contact parent
+
+     //   cardsContainer.removeChild(document.getElementById(targetId));
+
+     if(tagname=="BUTTON")   
+      {
+
+      return  cardsContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  
+    }
+      else{
+  
+        return  cardsContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  
+      }
+
+
+      //update Local localStorage
+
+
+};   
+
+
+
+
